2d77f7ad8d4117066d1f9172805c9b200e7ac353
AndrewHuang771 <ahuang771@gmail.com>
2019-01-12 15:08:55 -0500

    fixed not able to access http jquery

--- personalWebsite/javascript/art.js
+++ personalWebsite/javascript/art.js
@@ -3,7 +3,20 @@ $(document).ready( function() {
     let $element = $("<div>");
     $element.addClass("portrait");
     $element.attr("id", "portrait" + i);
-    $element.css({"background-image":"url('./portraits/portrait" + i + ".jpg')","height":"200px","width":"200px","background-size":"contain","background-repeat":"no-repeat", "display":"inline-block"});
+    $element.css({"background-image":"url('./portraits/portrait" + i + ".jpg')","height":"200px","width":"154.43px", "left": (200*i) + "px"});
     $("#displayBox").append( $element );
+
+    $("#portrait" + i ).on("mouseenter", function() {
+      console.log( i );
+      // $element.css({"width":"61.81vh","height":"80vh", "z-index":"100", "margin-top":"-40vh"})
+      $element.css({"transform":"scale(3)","z-index":"1000"});
+    });
+
+    $("#portrait" + i ).on("mouseleave", function() {
+      console.log( i );
+      // $element.css({"width":"200px","height":"200px", "z-index":"0",  "margin-top":"0vw"})
+      $element.css({"transform":"scale(1)","z-index":"0"});
+    });
   }
+
 });
