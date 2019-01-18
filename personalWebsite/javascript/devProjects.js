c0a68179b7ca24ff336456594be1990ca97854c9
AndrewHuang771 <ahuang771@gmail.com>
2019-01-17 19:47:51 -0500

    linked in univInfo

--- personalWebsite/javascript/devProjects.js
+++ personalWebsite/javascript/devProjects.js
@@ -12,7 +12,9 @@ function devProjectsHTML() {
     $("#mainContent").append( $projectElement );
   }
 
+
   $("#project2").attr("onclick", "window.location = './personalWebsite/projects/soundPlayer/template.html'");
+  $("#project1").attr("onclick", "window.location = 'https://github.com/AndrewHuang771/univInfo'");
   $("#project0").attr("onclick", "window.location = './personalWebsite/projects/soliloquy/appendix.html'");
   $("#project5").attr("onclick", "window.location = 'https://github.com/AndrewHuang771/DMOJ-Solutions'");
   $("#project4").attr("onclick", "window.location = './personalWebsite/projects/physics/chp1/index.html'");
--- personalWebsite/javascript/home.js
+++ personalWebsite/javascript/home.js
@@ -1,7 +1,15 @@
 function homeHTML() {
   let $back = $("<div>");
   $back.addClass("back");
-  let $back1 = $("<div>");
-  $back1.addClass("back1");
-  $("#mainContent").append( $back, $back1 );
+  $back.attr("id","back");
+  let $drag = $("<div>");
+  $drag.attr("id","backheader");
+  // let $back1 = $("<div>");
+  // $back1.addClass("back1");
+  // $back1.attr("id","back1");
+  // $back.prepend( $drag );
+  $("#mainContent").append( $back );
+
+  // dragElement(document.getElementById("back"));
+
 }
