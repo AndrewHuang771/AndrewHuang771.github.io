d1ccc9c1bdfb5e48227890564aa0065e6b15edd9
AndrewHuang771 <ahuang771@gmail.com>
2019-01-21 00:15:29 -0500

    changed font for cm

--- personalWebsite/javascript/devProjects.js
+++ personalWebsite/javascript/devProjects.js
@@ -6,7 +6,7 @@ function devProjectsHTML() {
   $mainWrapper.append( $textbox, $imagebox );
   $("#mainContent").append( $mainWrapper );
 
-  for ( let i = 0; i < 6; i ++ ) {
+  for ( let i = 0; i < 4; i ++ ) {
     let $projectElement = $("<div>");
     $projectElement.addClass("projectElement");
     $projectElement.attr("id", "project" + i);
@@ -14,10 +14,10 @@ function devProjectsHTML() {
   }
 
   $("#project2").attr("onclick", "window.location = './personalWebsite/projects/soundPlayer/template.html'");
-  // $("#project1").attr("onclick", "window.location = 'https://github.com/AndrewHuang771/univInfo'");
+  $("#project1").attr("onclick", "window.location = 'https://github.com/AndrewHuang771/univInfo'");
   $("#project0").attr("onclick", "window.location = './personalWebsite/projects/soliloquy/appendix.html'");
   $("#project5").attr("onclick", "window.location = 'https://github.com/AndrewHuang771/DMOJ-Solutions'");
-  // $("#project4").attr("onclick", "window.location = './personalWebsite/projects/physics/chp1/index.html'");
+  $("#project4").attr("onclick", "window.location = './personalWebsite/projects/physics/chp1/index.html'");
   $("#project3").attr("onclick", "window.location = 'https://github.com/AndrewHuang771/Huesja'");
 
   // $imagebox.css("background-image","utl('./personalWebsite/images/skyline.jpg')");
